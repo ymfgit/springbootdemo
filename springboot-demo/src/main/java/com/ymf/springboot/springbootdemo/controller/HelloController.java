c0854dc7fa810204891cae976618ef637002201f
姚美发 <yaomeifa@xinyusoft.com>
2018-07-24 09:45:42 +0800

    20180724 swagger2和log4j

--- springboot-demo/src/main/java/com/ymf/springboot/springbootdemo/controller/HelloController.java
+++ springboot-demo/src/main/java/com/ymf/springboot/springbootdemo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.ymf.springboot.springbootdemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @RequestMapping(value = "sayhello", method = RequestMethod.GET)
     public String hello() {
+        logger.info("hello world");
         return "Hello springboot4";
     }
 }
+
--- springboot-demo/src/main/java/com/ymf/springboot/springbootdemo/web/UserContoller.java
+++ springboot-demo/src/main/java/com/ymf/springboot/springbootdemo/web/UserContoller.java
@@ -3,6 +3,9 @@ package com.ymf.springboot.springbootdemo.web;
 import com.ymf.springboot.springbootdemo.bean.JsonResult;
 import com.ymf.springboot.springbootdemo.bean.User;
 import com.ymf.springboot.springbootdemo.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,11 +29,14 @@ public class UserContoller {
 
     /**
      * 根据ID查询用户
+     *
      * @param id
      * @return
      */
+    @ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "path")
     @RequestMapping(value = "user/{id}", method = RequestMethod.GET)
-    public ResponseEntity<JsonResult> getUserById (@PathVariable(value = "id") Integer id){
+    public ResponseEntity<JsonResult> getUserById(@PathVariable(value = "id") Integer id) {
         JsonResult r = new JsonResult();
         try {
             User user = userService.getUserById(id);
@@ -48,17 +54,14 @@ public class UserContoller {
     }
 
 
-
     /**
-
      * 查询用户列表
-
+     *
      * @return
-
      */
-
+    @ApiOperation(value = "获取用户列表", notes = "获取用户列表")
     @RequestMapping(value = "users", method = RequestMethod.GET)
-    public ResponseEntity<JsonResult> getUserList (){
+    public ResponseEntity<JsonResult> getUserList() {
         JsonResult r = new JsonResult();
 
         try {
@@ -77,14 +80,16 @@ public class UserContoller {
     }
 
 
-
     /**
      * 添加用户
+     *
      * @param user
      * @return
      */
+    @ApiOperation(value = "创建用户", notes = "根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @RequestMapping(value = "user", method = RequestMethod.POST)
-    public ResponseEntity<JsonResult> add (@RequestBody User user){
+    public ResponseEntity<JsonResult> add(@RequestBody User user) {
         JsonResult r = new JsonResult();
 
         try {
@@ -92,7 +97,6 @@ public class UserContoller {
             if (orderId < 0) {
                 r.setResult(orderId);
                 r.setStatus("fail");
-
             } else {
                 r.setResult(orderId);
                 r.setStatus("ok");
@@ -102,7 +106,6 @@ public class UserContoller {
             r.setResult(e.getClass().getName() + ":" + e.getMessage());
             r.setStatus("error");
             e.printStackTrace();
-
         }
         return ResponseEntity.ok(r);
 
@@ -110,12 +113,14 @@ public class UserContoller {
 
     /**
      * 根据id删除用户
+     *
      * @param id
      * @return
      */
-
+    @ApiOperation(value = "删除用户", notes = "根据url的id来指定删除用户")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path")
     @RequestMapping(value = "user/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<JsonResult> delete (@PathVariable(value = "id") Integer id){
+    public ResponseEntity<JsonResult> delete(@PathVariable(value = "id") Integer id) {
         JsonResult r = new JsonResult();
 
         try {
@@ -141,43 +146,38 @@ public class UserContoller {
     }
 
 
-
     /**
-
      * 根据id修改用户信息
-
+     *
      * @param user
-
      * @return
-
      */
+    @ApiOperation(value = "更新信息", notes = "根据url的id来指定更新用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path"),
+            @ApiImplicitParam(name = "user", value = "用户实体user", required = true, dataType = "User")
 
+    })
     @RequestMapping(value = "user/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<JsonResult> update (@PathVariable("id") Integer id, @RequestBody User user){
+    public ResponseEntity<JsonResult> update(@PathVariable("id") Integer id, @RequestBody User user) {
 
         JsonResult r = new JsonResult();
 
         try {
             int ret = userService.update(id, user);
-
             if (ret < 0) {
                 r.setResult(ret);
-
                 r.setStatus("fail");
 
             } else {
                 r.setResult(ret);
-
                 r.setStatus("ok");
-
             }
         } catch (Exception e) {
             r.setResult(e.getClass().getName() + ":" + e.getMessage());
             r.setStatus("error");
             e.printStackTrace();
-
         }
-
         return ResponseEntity.ok(r);
 
     }
